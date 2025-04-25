7c781ca72649b4933f7bf82a9af8b5edc22fb095
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-04-24 20:30:54 -0700

    Added firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { Firestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'flatFinder';
+export class AppComponent  {
+
+  constructor(private firestore: Firestore, private auth: Auth) {}
+
+  ngOnInit() {
+    this.login();
+  }
+
+  login() {
+    const email = 'user3@test.com';
+    const password = '1234abcd';
+
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        console.log('User logged in:', userCredential.user);
+      })
+      .catch((error) => {
+        console.error('Error logging in:', error);
+      });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { environment } from '../../environments/environment';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth())
+  ]
 };
