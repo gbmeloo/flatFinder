fe7fec0fc1b003287a229e22b4838d53aa934265
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-04-29 20:10:15 -0700

    Added routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { FlatsComponent } from './components/flats/flats.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/flats'
+    },
+    {
+        path: 'flats',
+        component: FlatsComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    }
+];
