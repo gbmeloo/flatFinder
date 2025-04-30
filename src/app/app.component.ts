6a3c47b78502982d82da901ef739a4c1331a577a
CauaBraga04 <kakabraga.cb.cb.cb@gmail.com>
2025-04-29 20:23:26 -0700

    Flats base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FlatsComponent } from "./components/flats/flats.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FlatsComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/flats/flats.component.ts
+++ src/app/components/flats/flats.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flats',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './flats.component.html',
   styleUrl: './flats.component.css'
 })
 export class FlatsComponent {
-
+   addToFavorites() {}
+   
+   flats = [{name: 'Flat 1', price: 100, imageUrl:"blank", location: "test", size: "10"},]
 }
