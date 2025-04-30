6fe8f10d3d4ad44464e3cddbd3e10b6c848889fe
CauaBraga04 <kakabraga.cb.cb.cb@gmail.com>
2025-04-29 19:48:50 -0700

    Added header component and router outlet to app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,16 @@
 import { Component } from '@angular/core';
-import { Auth, signInWithEmailAndPassword } from '@angular/fire/auth';
-import { Firestore } from '@angular/fire/firestore';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent  {
-  isLoggedIn: boolean = true;
+  
 
-  constructor(private firestore: Firestore, private auth: Auth) {}
-
-  ngOnInit() {
-    this.login();
-  }
-
-  login() {
-    const email = 'user3@test.com';
-    const password = '1234abcd';
-
-    signInWithEmailAndPassword(this.auth, email, password)
-      .then((userCredential) => {
-        console.log('User logged in:', userCredential.user);
-      })
-      .catch((error) => {
-        console.error('Error logging in:', error);
-      });
-  }
 }
