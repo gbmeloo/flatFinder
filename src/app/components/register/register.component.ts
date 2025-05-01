252dcd5acd1a02c8f183639383a300d4f6f0c23e
Gabriel Galvao <gabriel.mgalvao38@gmail.com>
2025-04-30 19:02:00 -0700

    Changes to be committed: Added styles to login and register components Added registration to firebase on register component

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
+
 @Component({
   selector: 'app-register',
   imports: [ReactiveFormsModule],
@@ -11,20 +13,32 @@ import { ReactiveFormsModule } from '@angular/forms';
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router) {
+  constructor(private fb: FormBuilder, private router: Router, private auth: Auth) {
     this.registerForm = this.fb.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
       dob: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
+<<<<<<< HEAD
       password: ['', [Validators.required, Validators.minLength(6)]]
+=======
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+>>>>>>> 5f11cdd (Changes to be committed:)
     });
   }
 
-  onSubmit() {
+  async onSubmit() {
     if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-      this.router.navigate(['/']);
+      const { email, password } = this.registerForm.value;
+      try {
+        await createUserWithEmailAndPassword(this.auth, email, password);
+        console.log('User registered successfully');
+        this.router.navigate(['/login']);
+      } catch (error) {
+        console.error('Error registering user:', error);
+      }
+      
     }
   }
 }
