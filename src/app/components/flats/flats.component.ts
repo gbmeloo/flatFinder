800453c8d679554c1bea606aeca6d87c6b8039f2
Chih-Yun Hsu <chihyunhsu1105@gmail.com>
2025-05-06 03:58:54 -0700

    Update flats style

--- src/app/components/flats/flats.component.ts
+++ src/app/components/flats/flats.component.ts
@@ -1,24 +1,188 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { jwtDecode } from 'jwt-decode';
+import { Firestore, collection, query, where, orderBy, getDocs, Timestamp  } from '@angular/fire/firestore';
+
+export interface Flat {
+  id: string;
+  city: string;
+  street_name: string;
+  street_number: string;
+  rent_price: number;
+  area_size: number;
+  has_ac: boolean;
+  year_built: number;
+  date_available: Timestamp;
+  date_available_display: string;
+  imageUrl: string;
+  landlord_id: string;
+  landlord_name: string;
+  landlord_email: string;
+}
 
 @Component({
   selector: 'app-flats',
-  imports: [CommonModule],
+  imports: [
+    MatIconModule,
+    CommonModule,
+    FormsModule
+  ],
   templateUrl: './flats.component.html',
   styleUrl: './flats.component.css'
 })
 export class FlatsComponent {
-   addToFavorites() {}
-   
-   flats = [{landlord_id: 'Joshua',
-    city: 'city 1',
-    rent_price: 100,
-    area_size: 200,
-    imageUrl: 'https://randomwordgenerator.com/img/picture-generator/54e5d2444253af14f1dc8460962e33791c3ad6e04e50744172287ad1954ec7_640.jpg',
-    has_ac: true, 
-    date_available: "2023-10-01",
-    street_number: 505,
-    street_name: "street A",
-    year_built: 1999
-  },]
+  userId: string | null = null;
+  allflats: Flat[] =[];
+
+  filteredFlats: Flat[] = [];
+  sortingClicked: string[] = [];
+  isAce_City = true;
+  isAce_Price = true;
+  isAce_Area = true;
+  filterCity: string = '';
+  minPrice: number = Infinity;
+  maxPrice: number = Infinity;
+  minArea: number = Infinity;
+  maxArea: number = Infinity;
+
+  constructor(private firestore: Firestore,) { }
+
+  ngOnInit() {
+    const token = localStorage.getItem('idToken');
+    if (token) {
+      try {
+        const decodedToken: any = jwtDecode(token);
+        this.userId = decodedToken.user_id;
+  
+        if (this.userId) {
+          this.getAllFlats();
+        } else {
+          console.error('User ID is null or undefined.');
+        }
+      } catch (error) {
+        console.error('Error decoding token:', error);
+      }
+    } else {
+      console.error('No token found in sessionStorage.');
+    }
+  }
+
+  async getAllFlats() {  
+    try {
+      const users = collection(this.firestore, 'users');
+      const u = query(users);
+      const queryUsers = await getDocs(u);
+
+      const flats = collection(this.firestore, 'flats');
+      const q = query(
+        flats,
+        orderBy('city', 'asc'),
+        orderBy('rent_price', 'asc'),
+        orderBy('area_size', 'asc')
+      );
+      const queryAll = await getDocs(q);
+      queryAll.forEach((f) => {
+        let flat: Flat = f.data() as Flat;
+        flat.id = f.id;
+        const firestoreDate: Timestamp = flat.date_available;
+        const jsDate = firestoreDate.toDate();
+        flat.date_available_display = jsDate.toLocaleDateString('en-CA');
+        flat.imageUrl = "https://cdngeneral.point2homes.com/dmslivecafe/3/1652463/20230203_231230669_iOS(20250312114430897).jpg?width=360&quality=80";
+        
+        const landlordDoc = queryUsers.docs.find(doc => doc.id === flat.landlord_id);
+        const landlord = landlordDoc ? landlordDoc.data() : null;
+        if(landlord != null) {
+          flat.landlord_name =`${String(landlord["firstName"] ?? '')} ${String(landlord["lastName"] ?? '')}`;
+          flat.landlord_email = String(landlord["email"] ?? '');
+        }
+        else {
+          flat.landlord_name = "";
+          flat.landlord_email = "";
+        }
+
+        console.log(flat);
+        this.allflats.push(flat);
+        this.filteredFlats.push(flat);
+      });
+    } catch (error) {
+      console.error('Error fetching my flats data:', error);
+    }
+  }
+
+  async onSort(field: string) {
+    try {
+      if (!this.sortingClicked.includes(field)) {
+        this.sortingClicked.push(field);
+      }
+
+      switch (field) {
+        case 'city':
+          this.isAce_City = !this.isAce_City;
+          break;    
+        case 'rent_price':
+          this.isAce_Price = !this.isAce_Price;
+          break;    
+        case 'area_size':
+          this.isAce_Area = !this.isAce_Area;
+          break;
+      }
+
+      await this.onFilter();
+    } catch (error) {
+      console.error('Error sorting flats data:', error);
+    }
+  }
+
+  async onFilter() {
+    try {
+      let haveFilter = false;
+      this.filteredFlats = this.allflats;
+
+      if(this.filterCity.length > 0) {
+        haveFilter = true;
+        this.filteredFlats = this.filteredFlats.filter(flat => flat.city?.toLowerCase().includes(this.filterCity.toLocaleLowerCase()));
+      }
+
+      if(this.minPrice != Infinity && this.minPrice != null) {
+        haveFilter = true;
+        this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price >= this.minPrice);
+      }
+      
+      if(this.maxPrice != Infinity && this.maxPrice != null) {
+        haveFilter = true;
+        this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price <= this.maxPrice);
+      }
+      
+      if(this.minArea != Infinity && this.minArea != null) {
+        haveFilter = true;
+        this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size >= this.minArea);
+      }
+      
+      if(this.maxArea != Infinity && this.maxArea != null) {
+        haveFilter = true;
+        this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size <= this.maxArea);
+      }
+
+      let direction = this.isAce_City ? 1 : -1;
+      this.filteredFlats.sort((a, b) => a.city.localeCompare(b.city) * direction);
+
+      direction = this.isAce_Price ? 1 : -1;
+      this.filteredFlats.sort((a, b) => (a.rent_price - b.rent_price) * direction);
+
+      direction = this.isAce_Area ? 1 : -1;
+      this.filteredFlats.sort((a, b) => (a.area_size - b.area_size) * direction);
+    } catch (error) {
+      console.error('Error filtering data:', error);
+    }
+  }
+
+  async messageFlat(id: string) {
+    
+  }
+
+  async favoriteFlat(id: string) {
+    
+  }
 }
