f9ead96b09fb1c9ff2e5d02b3027f92dd3ad28a4
CauaBraga04 <kakabraga.cb.cb.cb@gmail.com>
2025-05-06 20:24:57 -0700

    Fav Flat GG edition

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -33,9 +33,8 @@ export interface Flat {
   styleUrl: './favorites.component.css'
 })
 export class FavoritesComponent {
-  favoritesFlats: string[] = [];
+  favoritesFlats: Flat[] = [];
   userId: string | null = null;
-  allflats: Flat[] =[];
 
   filteredFlats: Flat[] = [];
   sortingClicked: string[] = [];
@@ -48,7 +47,7 @@ export class FavoritesComponent {
   minArea: number = Infinity;
   maxArea: number = Infinity;
 
-  constructor(private firestore: Firestore,) { }
+  constructor(private firestore: Firestore) { }
 
   ngOnInit() {
     const token = localStorage.getItem('idToken');
@@ -58,7 +57,7 @@ export class FavoritesComponent {
         this.userId = decodedToken.user_id;
   
         if (this.userId) {
-          this.getAllFlats();
+          this.favoriteFlats();
         } else {
           console.error('User ID is null or undefined.');
         }
@@ -70,180 +69,95 @@ export class FavoritesComponent {
     }
   }
 
-  async getAllFlats(): Promise<Flat[]> {  
-    try {
-      if (!this.userId) {
-        throw new Error('User ID is null or undefined.');
-      }
+  async favoriteFlats(): Promise<void> {  
+    if (this.userId) {
       const userDocRef = doc(this.firestore, 'users', this.userId);
-      const userDoc = await getDoc(userDocRef);
-      if (userDoc.exists()) {
-        const userData = userDoc.data();
-        const favorites: string[] = userData['favorites'] || [];
-        this.favoritesFlats.push(...favorites);
-        this.favoritesFlats = favorites
-        alert(this.favoritesFlats)
-      } else {
-        alert('User document does not exist.');
-        return [];
-      }
-
-      const users = collection(this.firestore, 'users');
-      const u = query(users);
-      const queryUsers = await getDocs(u);
-
-      const flats = collection(this.firestore, 'flats');
-      const q = query(
-        flats,
-        orderBy('city', 'asc'),
-        orderBy('rent_price', 'asc'),
-        orderBy('area_size', 'asc')
-      );
-      const queryAll = await getDocs(q);
-      queryAll.forEach((f) => {
-        let flat: Flat = f.data() as Flat;
-        flat.id = f.id;
-        const firestoreDate: Timestamp = flat.date_available;
-        const jsDate = firestoreDate.toDate();
-        flat.date_available_display = jsDate.toLocaleDateString('en-CA');
-        flat.imageUrl = "https://cdngeneral.point2homes.com/dmslivecafe/3/1652463/20230203_231230669_iOS(20250312114430897).jpg?width=360&quality=80";
-        
-        const landlordDoc = queryUsers.docs.find(doc => doc.id === flat.landlord_id);
-        const landlord = landlordDoc ? landlordDoc.data() : null;
-        if(landlord != null) {
-          flat.landlord_name =`${String(landlord["firstName"] ?? '')} ${String(landlord["lastName"] ?? '')}`;
-          flat.landlord_email = String(landlord["email"] ?? '');
-          
-        }
-        else {
-          flat.landlord_name = "";
-          flat.landlord_email = "";
-        }
-
-        this.favoritesFlats.forEach(flat => {
-
-        })
-        
-        for (let i: number = 0; i < this.favoritesFlats.length; i++) {
-          if (this.favoritesFlats[i] == flat.id) {
-            alert(flat.id + " is in favorites")
-            console.log(flat);
-            this.allflats.push(flat);
-            this.filteredFlats.push(flat);
-          }
-      }
-        
-      });
-    } catch (error) {
-      console.error('Error fetching my flats data:', error);
-      return [];
-    }
-    return this.allflats;
-  }
-
-  async onSort(field: string) {
-    try {
-      if (!this.sortingClicked.includes(field)) {
-        this.sortingClicked.push(field);
-      }
-
-      switch (field) {
-        case 'city':
-          this.isAce_City = !this.isAce_City;
-          break;    
-        case 'rent_price':
-          this.isAce_Price = !this.isAce_Price;
-          break;    
-        case 'area_size':
-          this.isAce_Area = !this.isAce_Area;
-          break;
-      }
-
-      await this.onFilter();
-    } catch (error) {
-      console.error('Error sorting flats data:', error);
-    }
-  }
-
-  async onFilter() {
-    try {
-      let haveFilter = false;
-      this.filteredFlats = this.allflats;
-
-      if(this.filterCity.length > 0) {
-        haveFilter = true;
-        this.filteredFlats = this.filteredFlats.filter(flat => flat.city?.toLowerCase().includes(this.filterCity.toLocaleLowerCase()));
-      }
-
-      if(this.minPrice != Infinity && this.minPrice != null) {
-        haveFilter = true;
-        this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price >= this.minPrice);
-      }
-      
-      if(this.maxPrice != Infinity && this.maxPrice != null) {
-        haveFilter = true;
-        this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price <= this.maxPrice);
-      }
-      
-      if(this.minArea != Infinity && this.minArea != null) {
-        haveFilter = true;
-        this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size >= this.minArea);
-      }
-      
-      if(this.maxArea != Infinity && this.maxArea != null) {
-        haveFilter = true;
-        this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size <= this.maxArea);
-      }
-
-      let direction = this.isAce_City ? 1 : -1;
-      this.filteredFlats.sort((a, b) => a.city.localeCompare(b.city) * direction);
-
-      direction = this.isAce_Price ? 1 : -1;
-      this.filteredFlats.sort((a, b) => (a.rent_price - b.rent_price) * direction);
-
-      direction = this.isAce_Area ? 1 : -1;
-      this.filteredFlats.sort((a, b) => (a.area_size - b.area_size) * direction);
-    } catch (error) {
-      console.error('Error filtering data:', error);
-    }
-  }
-
-  async messageFlat(id: string) {
-    
-  }
-
-  async favoriteFlat(id: string) {
-    try {
-      if (!this.userId) {
-        console.error('User is not logged in.');
-        return;
-      }
-  
-      const userDocRef = collection(this.firestore, 'users');
-      const userRef = doc(userDocRef, this.userId);
-  
-      // Fetch the user's current data
-      const userDoc = await getDoc(userRef);
-      if (userDoc.exists()) {
-        const userData = userDoc.data();
-        const favorites = userData['favorites'] || [];
-  
-        // Check if the flat is already in the favorites
-        if (!favorites.includes(id)) {
-          favorites.push(id);
-  
-          // Update the user's document with the new favorites array
-          await updateDoc(userRef, { favorites });
-          console.log(`Flat with ID ${id} added to favorites.`);
+      await getDoc(userDocRef).then(favoriteDoc => {
+        const data = favoriteDoc.data();
+        if (data) {
+          console.log('User favorites:', data['favorites']);
+          const favorites = data['favorites'] || [];
+          favorites.forEach(async (flatId: string) => {
+            const flatDocRef = doc(this.firestore, 'flats', flatId);
+            const flatDoc = await getDoc(flatDocRef);
+            if (flatDoc.exists()) {
+              const flatData = flatDoc.data() as Flat;
+              this.favoritesFlats.push(flatData);
+            }
+          });
+          console.log('Favorites Flats:', this.favoritesFlats);
         } else {
-          console.log(`Flat with ID ${id} is already in favorites.`);
+          console.error('User document data is undefined.');
         }
-      } else {
-        console.error('User document does not exist.');
-      }
+      })
+    }
   }
- catch (error: any) {
-  console.error('Error adding flat to favorites:', error);
-}
-}
+
+  // async onSort(field: string) {
+  //   try {
+  //     if (!this.sortingClicked.includes(field)) {
+  //       this.sortingClicked.push(field);
+  //     }
+
+  //     switch (field) {
+  //       case 'city':
+  //         this.isAce_City = !this.isAce_City;
+  //         break;    
+  //       case 'rent_price':
+  //         this.isAce_Price = !this.isAce_Price;
+  //         break;    
+  //       case 'area_size':
+  //         this.isAce_Area = !this.isAce_Area;
+  //         break;
+  //     }
+
+  //     await this.onFilter();
+  //   } catch (error) {
+  //     console.error('Error sorting flats data:', error);
+  //   }
+  // }
+
+  // async onFilter() {
+  //   try {
+  //     let haveFilter = false;
+  //     this.filteredFlats = this.allflats;
+
+  //     if(this.filterCity.length > 0) {
+  //       haveFilter = true;
+  //       this.filteredFlats = this.filteredFlats.filter(flat => flat.city?.toLowerCase().includes(this.filterCity.toLocaleLowerCase()));
+  //     }
+
+  //     if(this.minPrice != Infinity && this.minPrice != null) {
+  //       haveFilter = true;
+  //       this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price >= this.minPrice);
+  //     }
+      
+  //     if(this.maxPrice != Infinity && this.maxPrice != null) {
+  //       haveFilter = true;
+  //       this.filteredFlats = this.filteredFlats.filter(flat => flat.rent_price <= this.maxPrice);
+  //     }
+      
+  //     if(this.minArea != Infinity && this.minArea != null) {
+  //       haveFilter = true;
+  //       this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size >= this.minArea);
+  //     }
+      
+  //     if(this.maxArea != Infinity && this.maxArea != null) {
+  //       haveFilter = true;
+  //       this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size <= this.maxArea);
+  //     }
+
+  //     let direction = this.isAce_City ? 1 : -1;
+  //     this.filteredFlats.sort((a, b) => a.city.localeCompare(b.city) * direction);
+
+  //     direction = this.isAce_Price ? 1 : -1;
+  //     this.filteredFlats.sort((a, b) => (a.rent_price - b.rent_price) * direction);
+
+  //     direction = this.isAce_Area ? 1 : -1;
+  //     this.filteredFlats.sort((a, b) => (a.area_size - b.area_size) * direction);
+  //   } catch (error) {
+  //     console.error('Error filtering data:', error);
+  //   }
+  // }
+
 }
