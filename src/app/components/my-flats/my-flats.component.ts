e32e063891e70acb27664b5b6cd31cf1a4cb6b86
Chih-Yun Hsu <chihyunhsu1105@gmail.com>
2025-05-07 23:38:15 -0700

    Update filter style

--- src/app/components/my-flats/my-flats.component.ts
+++ src/app/components/my-flats/my-flats.component.ts
@@ -6,6 +6,10 @@ import { FormsModule } from '@angular/forms';
 import { jwtDecode } from 'jwt-decode';
 import { Firestore, collection, query, where, orderBy, getDocs, Timestamp, deleteDoc, doc  } from '@angular/fire/firestore';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { AuthService } from '../../services/auth.service';
+import { User } from 'firebase/auth';
+import { ChatService } from '../../services/chat.service';
+import { Router  } from '@angular/router';
 import { NotificationService } from '../../services/notification.service';
 
 export interface Flat {
@@ -24,6 +28,11 @@ export interface Flat {
   landlord_id: string;
   landlord_name: string;
   landlord_email: string;
+}
+
+export interface chk {
+  value: string;
+  checked: boolean;
 }
 
 @Component({
@@ -58,25 +67,51 @@ export interface Flat {
   styleUrl: './my-flats.component.css'
 })
 export class MyFlatsComponent {
-  userId: string | null = null;
+  showFilter: boolean = true;
+  isMobile: boolean = false; // Track if the screen is mobile size
+  
+  user: User | null = null;
   deleteError: string | null = null;
   deleteSuccess: string | null = null;
   loading: boolean = false;
-  isMobile: boolean = false; // Track if the screen is mobile size
-
   myflats: Flat[] =[];
+
   filteredFlats: Flat[] = [];
   sortingClicked: string[] = [];
-  isAce_City = true;
-  isAce_Price = true;
-  isAce_Area = true;
-  filterCity: string = '';
-  minPrice: number = Infinity;
-  maxPrice: number = Infinity;
-  minArea: number = Infinity;
-  maxArea: number = Infinity;
+  isAce_City: number = 0;
+  isAce_Price: number = 0;
+  isAce_Area: number = 0;
 
-  constructor(private firestore: Firestore, private notificationService: NotificationService) { }
+  filterCityList: chk[] = [
+    { value: 'toronto', checked: false },
+    { value: 'vancouver', checked: false },
+    { value: 'calgary', checked: false },
+    { value: 'montreal', checked: false },
+    { value: 'ottawa', checked: false }
+  ];
+  filterCity: string = '';
+
+  minPriceLimit: number = 0;
+  maxPriceLimit: number = 5000;
+  minPrice: number = this.minPriceLimit;
+  maxPrice: number = this.maxPriceLimit;
+  trackPriceLeft = '0%';
+  trackPriceWidth = '100%';
+
+  minAreaLimit: number = 1;
+  maxAreaLimit: number = 1000;
+  minArea: number = this.minAreaLimit;
+  maxArea: number = this.maxAreaLimit;
+  trackAreaLeft = '0%';
+  trackAreaWidth = '100%';
+
+  constructor(
+    private firestore: Firestore, 
+    private auth: AuthService,
+    private chatService: ChatService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) { }
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
@@ -85,42 +120,57 @@ export class MyFlatsComponent {
 
   checkScreenWidth(): void {
     this.isMobile = window.innerWidth <= 768; // You can adjust the breakpoint as needed
+    this.showFilter = !this.isMobile;
+  }
+
+  toggleFilter() {
+    this.showFilter = !this.showFilter;
   }
 
   ngOnInit() {
-    const token = localStorage.getItem('idToken');
-    if (token) {
-      try {
-        const decodedToken: any = jwtDecode(token);
-        this.userId = decodedToken.user_id;
-        if (this.userId) {
-          this.getMyFlats();
-        } else {
-          console.error('User ID is null or undefined.');
-        }
-      } catch (error) {
-        console.error('Error decoding token:', error);
+    this.auth.getCurrentUser().then(user => {
+      if (user) {
+        this.user = user;
+      } else {
+        console.log("User not logged in.")
       }
-    } else {
-      console.error('No token found in sessionStorage.');
-    }
+    })
+
+    this.getMyFlats();
   }
 
   async getMyFlats() {
-    if (!this.userId) {
-      console.error('User ID is null or undefined. Cannot fetch my flats data.');
+    console.log(this.user);
+    if (!this.user) {
+      console.error('User ID is null or undefined. Cannot fetch user data.');
       return;
     }
   
     try {
       const flats = collection(this.firestore, 'flats');
       const q = query(flats, 
-        where('landlord_id', '==', this.userId),
+        where('landlord_id', '==', this.user?.uid),
         orderBy('city', 'asc'),
         orderBy('rent_price', 'asc'),
         orderBy('area_size', 'asc')
       );
       const queryByUid = await getDocs(q);
+      
+      const flatsmap = queryByUid.docs.map(doc => doc.data());
+      const prices = flatsmap.map(flat => flat["rent_price"]);
+      this.minPriceLimit = Math.min(...prices);
+      this.maxPriceLimit = Math.max(...prices);
+      this.minPrice = this.minPriceLimit;
+      this.maxPrice = this.maxPriceLimit;
+      this.onPriceRangeChange();
+      
+      const areas = flatsmap.map(flat => flat["area_size"]);
+      this.minAreaLimit = Math.min(...areas);
+      this.maxAreaLimit = Math.max(...areas);
+      this.minArea = this.minAreaLimit;
+      this.maxArea = this.maxAreaLimit;
+      this.onAreaRangeChange();
+
       queryByUid.forEach((f) => {
         let flat: Flat = f.data() as Flat;
         flat.id = f.id;
@@ -136,6 +186,42 @@ export class MyFlatsComponent {
     }
   }
 
+  onFilterCityChecked(index: number, event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.filterCityList[index].checked = input.checked;
+    this.onFilter();
+  }
+
+  onPriceRangeChange() {
+    if (this.minPrice > this.maxPrice) {
+      [this.minPrice, this.maxPrice] = [this.maxPrice, this.minPrice];
+    }
+  
+    const total = this.maxPriceLimit - this.minPriceLimit;
+    const leftPercent = ((this.minPrice - this.minPriceLimit) / total) * 100;
+    const widthPercent = ((this.maxPrice - this.minPrice) / total) * 100;
+  
+    this.trackPriceLeft = `${leftPercent}%`;
+    this.trackPriceWidth = `${widthPercent}%`;
+
+    this.onFilter();
+  }
+
+  onAreaRangeChange() {
+    if (this.minArea > this.maxArea) {
+      [this.minArea, this.maxArea] = [this.maxArea, this.minArea];
+    }
+  
+    const total = this.maxAreaLimit - this.minAreaLimit;
+    const leftPercent = ((this.minArea - this.minAreaLimit) / total) * 100;
+    const widthPercent = ((this.maxArea - this.minArea) / total) * 100;
+  
+    this.trackAreaLeft = `${leftPercent}%`;
+    this.trackAreaWidth = `${widthPercent}%`;
+
+    this.onFilter();
+  }
+
   async onSort(field: string) {
     try {
       if (!this.sortingClicked.includes(field)) {
@@ -144,13 +230,13 @@ export class MyFlatsComponent {
 
       switch (field) {
         case 'city':
-          this.isAce_City = !this.isAce_City;
+          this.isAce_City = this.isAce_City == 1 ? -1 : 1;
           break;    
         case 'rent_price':
-          this.isAce_Price = !this.isAce_Price;
+          this.isAce_Price = this.isAce_Price == 1 ? -1 : 1;
           break;    
         case 'area_size':
-          this.isAce_Area = !this.isAce_Area;
+          this.isAce_Area = this.isAce_Area == 1 ? -1 : 1;
           break;
       }
 
@@ -164,6 +250,14 @@ export class MyFlatsComponent {
     try {
       let haveFilter = false;
       this.filteredFlats = this.myflats;
+
+      this.filterCityList.forEach(ck => {
+        if(ck.checked) {
+          haveFilter = true;
+          let city = ck.value.toLocaleLowerCase();
+          this.filteredFlats = this.filteredFlats.filter(flat => flat.city?.toLowerCase().includes(city));
+        }
+      });
 
       if(this.filterCity.length > 0) {
         haveFilter = true;
@@ -190,14 +284,25 @@ export class MyFlatsComponent {
         this.filteredFlats = this.filteredFlats.filter(flat => flat.area_size <= this.maxArea);
       }
 
-      let direction = this.isAce_City ? 1 : -1;
-      this.filteredFlats.sort((a, b) => a.city.localeCompare(b.city) * direction);
+      this.filteredFlats.sort((a, b) => {
+        let result = 0;
+        
+        if(this.isAce_City != 0) {
+          result = a.city.localeCompare(b.city) * this.isAce_City;
+          if (result !== 0) return result;
+        }
+      
+        if(this.isAce_Price != 0) {
+          result = (a.rent_price - b.rent_price) * this.isAce_Price;
+          if (result !== 0) return result;
+        }
+      
+        if(this.isAce_Area != 0) {
+          return (a.area_size - b.area_size) * this.isAce_Area;
+        }
 
-      direction = this.isAce_Price ? 1 : -1;
-      this.filteredFlats.sort((a, b) => (a.rent_price - b.rent_price) * direction);
-
-      direction = this.isAce_Area ? 1 : -1;
-      this.filteredFlats.sort((a, b) => (a.area_size - b.area_size) * direction);
+        return result;
+      });
     } catch (error) {
       console.error('Error filtering data:', error);
     }
@@ -243,7 +348,21 @@ export class MyFlatsComponent {
     }
   }
 
-  async messageFlat(id: string) {
-    
+  async messageFlat(flatId: string, landlordId: string, street: string, userId: string) {
+    if (userId === landlordId) {
+      this.notificationService.showNotification(
+        "Cannot start a conversation with yourself",
+        "Dimiss",
+        10000
+      )
+      return;
+    }
+    const chatId = await this.chatService.startNewChat(flatId, landlordId, street, userId);
+    if (chatId) {
+      // Redirect to the chat component with the chat ID
+      this.router.navigate(['/chat', chatId]);
+    } else {
+      console.error('Failed to start or retrieve chat.');
+    }
   }
 }
